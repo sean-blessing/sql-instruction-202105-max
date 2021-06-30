5329eeafc10586edc1a2ed82c160a8ede5ff6dc4
Sean Blessing <snblessing@gmail.com>
2021-06-30 11:27:40 -0400

    catchup commit

--- my-sql/food-db/food-db-recalculate-total-1.sql
+++ my-sql/food-db/food-db-recalculate-total-1.sql
@@ -2,11 +2,23 @@
 set @order_total = (select sum(price * Quantity) as total
   from lineitem
   join menuitem mi    on MenuItemID = mi.id
- where orderticketid = 2
+ where orderticketid = 1
  group by OrderTicketID);
 
 update orderticket
     set total = @order_total
-where id = 2;
+where id = 1;
 
 select * from orderticket;
+
+-- (select sum(price * Quantity) as total
+--   from lineitem
+--   join menuitem mi    on MenuItemID = mi.id
+--  where orderticketid = 1
+--  group by OrderTicketID);
+--  
+--  select * 
+--    from lineitem li
+--    join menuitem mi
+--      on MenuItemID = mi.id
+--  where OrderTicketID = 1;
